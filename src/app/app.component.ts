5e949557baa92e47b46f83468ec09f87dcc44a33
Paul Z <ziolkowski28paul@gmail.com>
2022-05-19 12:33:58 +0200

    Cleaning, Accessibility

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AuthorSource } from './core/author/author.source';
 import { ArticleSource } from './core/article/article.source';
 import {environment} from '../environments/environment';
+import { Title, Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -11,12 +12,26 @@ export class AppComponent implements OnInit {
   title = 'simple-app'
   public appVersion: string;
 
-  constructor (private authorSource: AuthorSource, private articleSource: ArticleSource ) { }
+  constructor (private authorSource: AuthorSource, private articleSource: ArticleSource, private metaTagService: Meta ) { }
 
   ngOnInit() {
     this.authorSource.preload().subscribe();
     this.articleSource.preload().subscribe();
     this.appVersion = environment.appVersion;
+    
+    this.metaTagService.addTags([
+      {
+        name: 'keywords',
+        content: 'Simple PWA',
+      },
+      {
+        name: 'description',
+        content: 'Simple PWA using Angular !',
+      },
+      { name: 'robots', content: 'index, follow' },
+      { name: 'author', content: 'Paul ZIOLKOWSKI' },
+      { charset: 'UTF-8' },
+    ]);
   }
   
 }
